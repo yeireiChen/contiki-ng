afc891682e24c8d114c4aef4b5e4fadc882e46d6
LookLikeAName <yl301l4ul6@gmail.com>
2018-08-10 17:46:18 +0800

    coap server RPL classic

--- os/projects/QoS_Test/coap-server-node/resources/res-bcollect.c
+++ os/projects/QoS_Test/coap-server-node/resources/res-bcollect.c
@@ -10,8 +10,7 @@
 #include "coap-engine.h"
 
 
-#include "net/routing/rpl-lite/rpl.h"
-#include "net/routing/rpl-lite/rpl-dag.h"
+#include "net/routing/rpl-classic/rpl.h"
 #include "net/routing/routing.h"
 #include "net/link-stats.h"
 
