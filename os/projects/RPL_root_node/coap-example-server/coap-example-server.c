5609aa0baf89fe0afa83117bc9cf3733dd1e7cb8
LookLikeAName <yl301l4ul6@gmail.com>
2018-09-11 13:14:08 +0800

    coap test

--- os/projects/RPL_root_node/coap-example-server/coap-example-server.c
+++ os/projects/RPL_root_node/coap-example-server/coap-example-server.c
@@ -52,97 +52,49 @@
 #include "sys/log.h"
 #define LOG_MODULE "App"
 #define LOG_LEVEL LOG_LEVEL_APP
-/*
- * Resources to be activated need to be imported through the extern keyword.
- * The build system automatically compiles the resources in the corresponding sub-directory.
- */
-extern coap_resource_t
-  res_hello,
-  res_mirror,
-  res_chunks,
-  res_separate,
-  res_push,
-  res_event,
-  res_sub,
-  res_b1_sep_b2;
-#if PLATFORM_HAS_LEDS
-extern coap_resource_t res_leds, res_toggle;
-#endif
-#if PLATFORM_HAS_LIGHT
-#include "dev/light-sensor.h"
-extern coap_resource_t res_light;
-#endif
-#if PLATFORM_HAS_BATTERY
-#include "dev/battery-sensor.h"
-extern coap_resource_t res_battery;
-#endif
-#if PLATFORM_HAS_TEMPERATURE
-#include "dev/temperature-sensor.h"
-extern coap_resource_t res_temperature;
-#endif
 
 PROCESS(er_example_server, "Erbium Example Server");
 AUTOSTART_PROCESSES(&er_example_server);
 
+/*---------------------------------------------------------------------------*/
+/*
+ * Resources to be activated need to be imported through the extern keyword.
+ * The build system automatically compiles the resources in the corresponding sub-directory.
+ */
+ extern coap_resource_t
+ res_hello,
+ res_separate,
+ res_push,
+ res_event,
+ res_toggle;
+ /*res_bcollect,
+ res_bcollect_2;*/
+ 
+
 PROCESS_THREAD(er_example_server, ev, data)
 {
-  PROCESS_BEGIN();
+ PROCESS_BEGIN();
 
-  PROCESS_PAUSE();
+ PROCESS_PAUSE();
 
-  LOG_INFO("Starting Erbium Example Server\n");
+ LOG_INFO("Starting Erbium Example Server\n");
 
-  /*
-   * Bind the resources to their Uri-Path.
-   * WARNING: Activating twice only means alternate path, not two instances!
-   * All static variables are the same for each URI path.
-   */
-  coap_activate_resource(&res_hello, "test/hello");
-  coap_activate_resource(&res_mirror, "debug/mirror");
-  coap_activate_resource(&res_chunks, "test/chunks");
-  coap_activate_resource(&res_separate, "test/separate");
-  coap_activate_resource(&res_push, "test/push");
-#if PLATFORM_HAS_BUTTON
-  coap_activate_resource(&res_event, "sensors/button");
-#endif /* PLATFORM_HAS_BUTTON */
-  coap_activate_resource(&res_sub, "test/sub");
-  coap_activate_resource(&res_b1_sep_b2, "test/b1sepb2");
-#if PLATFORM_HAS_LEDS
-/*  coap_activate_resource(&res_leds, "actuators/leds"); */
-  coap_activate_resource(&res_toggle, "actuators/toggle");
-#endif
-#if PLATFORM_HAS_LIGHT
-  coap_activate_resource(&res_light, "sensors/light");
-  SENSORS_ACTIVATE(light_sensor);
-#endif
-#if PLATFORM_HAS_BATTERY
-  coap_activate_resource(&res_battery, "sensors/battery");
-  SENSORS_ACTIVATE(battery_sensor);
-#endif
-#if PLATFORM_HAS_TEMPERATURE
-  coap_activate_resource(&res_temperature, "sensors/temperature");
-  SENSORS_ACTIVATE(temperature_sensor);
-#endif
+ /*
+  * Bind the resources to their Uri-Path.
+  * WARNING: Activating twice only means alternate path, not two instances!
+  * All static variables are the same for each URI path.
+  */
+ coap_activate_resource(&res_hello, "test/hello");
+ coap_activate_resource(&res_separate, "test/separate");
+ coap_activate_resource(&res_push, "test/push");
+ coap_activate_resource(&res_toggle, "actuators/toggle");
+ //coap_activate_resource(&res_bcollect, "g/bcollect");
+ //coap_activate_resource(&res_bcollect_2, "g/bcollect_2");
 
-  /* Define application-specific events here. */
-  while(1) {
-    PROCESS_WAIT_EVENT();
-#if PLATFORM_HAS_BUTTON
-#if PLATFORM_SUPPORTS_BUTTON_HAL
-    if(ev == button_hal_release_event) {
-#else
-    if(ev == sensors_event && data == &button_sensor) {
-#endif
-      LOG_DBG("*******BUTTON*******\n");
+ /* Define application-specific events here. */
+ while(1) {
+   PROCESS_WAIT_EVENT();
+ } 
 
-      /* Call the event_handler for this application-specific event. */
-      res_event.trigger();
-
-      /* Also call the separate response example handler. */
-      res_separate.resume();
-    }
-#endif /* PLATFORM_HAS_BUTTON */
-  }                             /* while (1) */
-
-  PROCESS_END();
+ PROCESS_END();
 }
