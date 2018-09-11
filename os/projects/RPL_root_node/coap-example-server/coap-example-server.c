7063998349e1a1af08fca6e45865bbe6e2e2474e
LookLikeAName <yl301l4ul6@gmail.com>
2018-09-11 13:18:37 +0800

    coap test

--- os/projects/RPL_root_node/coap-example-server/coap-example-server.c
+++ os/projects/RPL_root_node/coap-example-server/coap-example-server.c
@@ -42,6 +42,12 @@
 #include "contiki.h"
 #include "coap-engine.h"
 
+#if PLATFORM_SUPPORTS_BUTTON_HAL
+#include "dev/button-hal.h"
+#else
+#include "dev/button-sensor.h"
+#endif
+
 /* Log configuration */
 #include "sys/log.h"
 #define LOG_MODULE "App"
@@ -59,7 +65,21 @@ extern coap_resource_t
   res_event,
   res_sub,
   res_b1_sep_b2;
-
+#if PLATFORM_HAS_LEDS
+extern coap_resource_t res_leds, res_toggle;
+#endif
+#if PLATFORM_HAS_LIGHT
+#include "dev/light-sensor.h"
+extern coap_resource_t res_light;
+#endif
+#if PLATFORM_HAS_BATTERY
+#include "dev/battery-sensor.h"
+extern coap_resource_t res_battery;
+#endif
+#if PLATFORM_HAS_TEMPERATURE
+#include "dev/temperature-sensor.h"
+extern coap_resource_t res_temperature;
+#endif
 
 PROCESS(er_example_server, "Erbium Example Server");
 AUTOSTART_PROCESSES(&er_example_server);
@@ -82,11 +102,46 @@ PROCESS_THREAD(er_example_server, ev, data)
   coap_activate_resource(&res_chunks, "test/chunks");
   coap_activate_resource(&res_separate, "test/separate");
   coap_activate_resource(&res_push, "test/push");
+#if PLATFORM_HAS_BUTTON
+  coap_activate_resource(&res_event, "sensors/button");
+#endif /* PLATFORM_HAS_BUTTON */
+  coap_activate_resource(&res_sub, "test/sub");
+  coap_activate_resource(&res_b1_sep_b2, "test/b1sepb2");
+#if PLATFORM_HAS_LEDS
+/*  coap_activate_resource(&res_leds, "actuators/leds"); */
+  coap_activate_resource(&res_toggle, "actuators/toggle");
+#endif
+#if PLATFORM_HAS_LIGHT
+  coap_activate_resource(&res_light, "sensors/light");
+  SENSORS_ACTIVATE(light_sensor);
+#endif
+#if PLATFORM_HAS_BATTERY
+  coap_activate_resource(&res_battery, "sensors/battery");
+  SENSORS_ACTIVATE(battery_sensor);
+#endif
+#if PLATFORM_HAS_TEMPERATURE
+  coap_activate_resource(&res_temperature, "sensors/temperature");
+  SENSORS_ACTIVATE(temperature_sensor);
+#endif
 
   /* Define application-specific events here. */
   while(1) {
     PROCESS_WAIT_EVENT();
+#if PLATFORM_HAS_BUTTON
+#if PLATFORM_SUPPORTS_BUTTON_HAL
+    if(ev == button_hal_release_event) {
+#else
+    if(ev == sensors_event && data == &button_sensor) {
+#endif
+      LOG_DBG("*******BUTTON*******\n");
 
+      /* Call the event_handler for this application-specific event. */
+      res_event.trigger();
+
+      /* Also call the separate response example handler. */
+      res_separate.resume();
+    }
+#endif /* PLATFORM_HAS_BUTTON */
   }                             /* while (1) */
 
   PROCESS_END();
