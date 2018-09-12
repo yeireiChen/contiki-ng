0d5b357aabd0d49da9af925f3445c43bdcbf920d
LookLikeAName <yl301l4ul6@gmail.com>
2018-09-12 10:33:58 +0800

    add bcollect

--- os/projects/RPL_root_node/coap-example-server/resources/res-bcollect.c
+++ os/projects/RPL_root_node/coap-example-server/resources/res-bcollect.c
@@ -13,6 +13,7 @@
 #include "net/routing/rpl-lite/rpl-icmp6.h"
 #include "net/routing/rpl-lite/rpl-types.h"
 #include "net/routing/rpl-lite/rpl-dag.h"
+#include "net/routing/rpl-lite/uip.h"
 #include "net/routing/routing.h"
 #include "net/link-stats.h"
 
