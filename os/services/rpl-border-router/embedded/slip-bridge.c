238219bc9f5ddc416e4d9dd3161323637bea434f
LookLikeAName <yl301l4ul6@gmail.com>
2018-09-17 13:19:17 +0800

    add debug printf macro

--- os/services/rpl-border-router/embedded/slip-bridge.c
+++ os/services/rpl-border-router/embedded/slip-bridge.c
@@ -53,6 +53,16 @@
 #include "sys/log.h"
 #define LOG_MODULE "SLIP"
 #define LOG_LEVEL LOG_LEVEL_NONE
+
+#define DEBUG 1
+#if DEBUG
+#include <stdio.h>
+#define PRINTF(...) printf(__VA_ARGS__)
+#define PRINT6ADDR(addr) PRINTF("[%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x]", ((uint8_t *)addr)[0], ((uint8_t *)addr)[1], ((uint8_t *)addr)[2], ((uint8_t *)addr)[3], ((uint8_t *)addr)[4], ((uint8_t *)addr)[5], ((uint8_t *)addr)[6], ((uint8_t *)addr)[7], ((uint8_t *)addr)[8], ((uint8_t *)addr)[9], ((uint8_t *)addr)[10], ((uint8_t *)addr)[11], ((uint8_t *)addr)[12], ((uint8_t *)addr)[13], ((uint8_t *)addr)[14], ((uint8_t *)addr)[15])
+#else
+#define PRINTF(...)
+#define PRINT6ADDR(addr)
+#endif
 /*---------------------------------------------------------------------------*/
 void set_prefix_64(uip_ipaddr_t *);
 
