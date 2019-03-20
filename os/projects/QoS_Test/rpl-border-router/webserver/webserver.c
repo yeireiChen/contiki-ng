410fe68925bfe94a07a9afb7a5037afea15eb7d4
LookLikeAName <yl301l4ul6@gmail.com>
2019-03-20 10:31:43 +0800

    add topology print

--- os/projects/QoS_Test/rpl-border-router/webserver/webserver.c
+++ os/projects/QoS_Test/rpl-border-router/webserver/webserver.c
@@ -41,6 +41,17 @@
  /*---------------------------------------------------------------------------*/
  static const char *TOP = "<html>\n  <head>\n    <title>Contiki-NG</title>\n  </head>\n<body>\n";
  static const char *BOTTOM = "\n</body>\n</html>\n";
+static const char *SCRIPT_0 ="\n<script>\nvar links=Array.from(document.getElementsByClassName('link'));\nvar tree=[];\nvar nodes=[];\nvar root=new Object;\n";
+static const char *SCRIPT_1 ="root.node_addr=document.getElementById('root').innerHTML;\nroot.parent_addr ='';\nroot.child_list=[];\ntree.push(root);\n";
+static const char *SCRIPT_2 ="nodes=links.map(link=>{var node = new Object();link_data=link.innerHTML.replace('(','').replace(')','').split(' ');\n";
+static const char *SCRIPT_3 ="node.node_addr = link_data[0];node.parent_addr = link_data[2];node.child_list=[];return node;});tree=tree.concat(nodes);\n";
+static const char *SCRIPT_4 ="for(i=0;i<tree.length;i++){for(j=0;j<tree.length;j++){if(tree[i].node_addr==tree[j].parent_addr){tree[i].child_list.push(tree[j]);}}}\n";
+static const char *SCRIPT_5 ="var stack=[];var space_count=1;var current_node=[];var optStr=['Topology<br><br>'];stack.push(root);while(stack.length!=0){\n";
+static const char *SCRIPT_6 ="temp=stack.pop();if(current_node[current_node.length-1]!=temp.parent_addr){current_node.pop();space_count--;}tempStr='';\n";
+static const char *SCRIPT_7 ="for(i=0;i<space_count;i++){tempStr+='----';}tempStr+=temp.node_addr+'<br>';optStr.push(tempStr);\n";
+static const char *SCRIPT_8 ="if(temp.child_list.length>0){space_count++;current_node.push(temp.node_addr);}for(i=0;i<temp.child_list.length;i++){\n";
+static const char *SCRIPT_9 ="stack.push(temp.child_list[i]);}}optStr=optStr.join('');var topo=document.createElement('div');topo.innerHTML=optStr;\n";
+static const char *SCRIPT_10 ="document.body.appendChild(topo);console.log(optStr,document.body);\n</script>";
  static char buf[256];
  static int blen;
  #define ADD(...) do {                                                   \
@@ -91,7 +102,7 @@
    ADD("  Root\n  <ul>\n");
    uip_ipaddr_t hostaddr;
    NETSTACK_ROUTING.get_root_ipaddr(&hostaddr);
-   ADD("    <li>");
+   ADD("    <li id='root'>");
    ipaddr_add(&hostaddr);
    ADD("</li>\n");
    SEND(&s->sout);
@@ -158,9 +169,19 @@
      SEND(&s->sout);
    }
  #endif /* UIP_SR_LINK_NUM != 0 */
- 
+   SEND_STRING(&s->sout, SCRIPT_0);
+   SEND_STRING(&s->sout, SCRIPT_1);
+   SEND_STRING(&s->sout, SCRIPT_2);
+   SEND_STRING(&s->sout, SCRIPT_3);
+   SEND_STRING(&s->sout, SCRIPT_4);
+   SEND_STRING(&s->sout, SCRIPT_5);
+   SEND_STRING(&s->sout, SCRIPT_6);
+   SEND_STRING(&s->sout, SCRIPT_7);
+   SEND_STRING(&s->sout, SCRIPT_8);
+   SEND_STRING(&s->sout, SCRIPT_9);
+   SEND_STRING(&s->sout, SCRIPT_10);
    SEND_STRING(&s->sout, BOTTOM);
- 
+   
    PSOCK_END(&s->sout);
  }
  /*---------------------------------------------------------------------------*/
