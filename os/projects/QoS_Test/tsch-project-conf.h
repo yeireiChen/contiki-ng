e57ee7832fac8aab8996d416c812b73352e937d6
LookLikeAName <yl301l4ul6@gmail.com>
2019-03-22 12:12:53 +0800

    change log level & CoAP refresh interval

--- os/projects/QoS_Test/tsch-project-conf.h
+++ os/projects/QoS_Test/tsch-project-conf.h
@@ -100,6 +100,7 @@
 
 #endif /* WITH_SECURITY */
 
+#define COAP_OBSERVE_REFRESH_INTERVAL 9999
 /*******************************************************/
 /*************** Enable Orchestra **********************/
 /*******************************************************/
@@ -175,13 +176,13 @@
 /*******************************************************/
 
 /* Logging */
-#define LOG_CONF_LEVEL_MAIN                        LOG_LEVEL_INFO
-#define LOG_CONF_LEVEL_RPL                         LOG_LEVEL_INFO
+#define LOG_CONF_LEVEL_MAIN                        LOG_LEVEL_WARN
+#define LOG_CONF_LEVEL_RPL                         LOG_LEVEL_WARN
 #define LOG_CONF_LEVEL_TCPIP                       LOG_LEVEL_WARN
 #define LOG_CONF_LEVEL_IPV6                        LOG_LEVEL_WARN
 #define LOG_CONF_LEVEL_6LOWPAN                     LOG_LEVEL_WARN
 #define LOG_CONF_LEVEL_COAP                        LOG_LEVEL_WARN
-#define LOG_CONF_LEVEL_MAC                         LOG_LEVEL_INFO
+#define LOG_CONF_LEVEL_MAC                         LOG_LEVEL_WARN
 #define LOG_CONF_LEVEL_FRAMER                      LOG_LEVEL_WARN
 #define TSCH_LOG_CONF_PER_SLOT                     0
 
