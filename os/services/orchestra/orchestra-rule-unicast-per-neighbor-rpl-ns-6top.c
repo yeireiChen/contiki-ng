0e06e97050020c1fc3211647bc22de7ef9d7e305
unknown <yl301l4ul6@gmail.com>
2019-03-26 14:48:48 +0800

    change make file & include path

--- os/services/orchestra/orchestra-rule-unicast-per-neighbor-rpl-ns-6top.c
+++ os/services/orchestra/orchestra-rule-unicast-per-neighbor-rpl-ns-6top.c
@@ -43,7 +43,7 @@
 #include "net/ipv6/uip-ds6-route.h"
 #include "net/packetbuf.h"
 #include "net/mac/tsch/sixtop/sixtop.h"
-#include "/project/sixtop_on_orchestra/sixtop_schdule/sf-simple.h"
+#include "projects/sixtop_on_orchestra/sixtop_schdule/sf-simple.h"
 
 static uint16_t slotframe_handle = 0;
 static uint16_t channel_offset = 0;
