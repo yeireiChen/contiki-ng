379ad26956d9a100fbcf7df65ddda8a27caeb8e4
unknown <yl301l4ul6@gmail.com>
2019-03-28 16:23:11 +0800

    child log test

--- os/net/ipv6/uip6.c
+++ os/net/ipv6/uip6.c
@@ -1318,20 +1318,24 @@ void uip_process(uint8_t flag)
           {
             LOG_INFO_("HEY ");
             LOG_INFO_6ADDR(&UIP_IP_BUF->srcipaddr);
-            LOG_INFO_(" OR ");
-            uip_lladdr_t *lladdr;
-            uip_ds6_set_lladdr_from_iid(lladdr,&UIP_IP_BUF->srcipaddr);
-            LOG_INFO_LLADDR((const linkaddr_t *)lladdr);
-            child_node *node;
-            node = child_list_add_child((linkaddr_t *)lladdr);
-            if(node != NULL){
-            LOG_INFO_(", NOW YOU ARE");
-            LOG_INFO_LLADDR((const linkaddr_t *)node->address);
-            LOG_INFO_("& I'M YOUR FATHER!!!\n");
+            uip_lladdr_t *lladdr = NULL;
+            uip_ds6_set_lladdr_from_iid(lladdr, &UIP_IP_BUF->srcipaddr);
+            if (lladdr != NULL)
+            {
+              LOG_INFO_(" OR ");
+              LOG_INFO_LLADDR((const linkaddr_t *)lladdr);
+              child_node *node;
+              node = child_list_add_child((linkaddr_t *)lladdr);
+              if (node != NULL)
+              {
+                LOG_INFO_(", NOW YOU ARE");
+                LOG_INFO_LLADDR((const linkaddr_t *)node->address);
+                LOG_INFO_("& I'M YOUR FATHER!!!\n");
+              }
             }
             else
             {
-              LOG_INFO_(", SOMETHING WENT WRONG!!!\n");
+              LOG_INFO_(",OH! SOMETHING WENT WRONG!!!\n");
             }
           }
         }
