ded2a43dd9b996075685c7df1f396a0e0e9effb9
unknown <yl301l4ul6@gmail.com>
2019-04-11 10:29:37 +0800

    debug

--- projects/sixtop_on_orchestra/tsch-project-conf.h
+++ projects/sixtop_on_orchestra/tsch-project-conf.h
@@ -139,6 +139,7 @@
 #endif /* WITH_ORCHESTRA */
 
 #define SF_CONF_SIX_TOP_SLOTFRAME_LENGTH ORCHESTRA_CONF_SIXTOP_PERIOD
+#define SIXTOP_CONF_MAX_TRANSACTIONS      3
 
 #if CONTIKI_TARGET_Z1
 /* Save some space to fit the limited RAM of the z1 */
