bcd93ffa9cbc3dfb483f486bee11cf69be4797ed
unknown <yl301l4ul6@gmail.com>
2019-04-11 12:48:50 +0800

    debug

--- os/moudle/sixtop_simple_schdule/sf-simple.c
+++ os/moudle/sixtop_simple_schdule/sf-simple.c
@@ -632,13 +632,13 @@ response_input(sixp_pkt_rc_t rc,
         print_cell_list(cell_list, cell_list_len);
         LOG_INFO("\n");
         //add_links_to_schedule(peer_addr, LINK_OPTION_RX,&cell_list[1], cell_list_len-1);
-        read_cell(&cell_list+sizeof(sf_simple_cell_t), &cell);
+        read_cell((const uint8_t *)(&cell_list+sizeof(sf_simple_cell_t)), &cell);
         LOG_INFO("sf-simple: realocate to slot_offset: %d ,channel_offset %d",cell.timeslot_offset,cell.channel_offset);
         node = find_child(peer_addr);
         if(node){
           child_list_set_child_offsets(node,cell.timeslot_offset,cell.channel_offset);
         }
-        read_cell(&cell_list, &cell);
+        read_cell((const uint8_t *)&cell_list, &cell);
         LOG_INFO(" frome slot_offset: %d ,channel_offset %d\n",cell.timeslot_offset,cell.channel_offset);
         if(!slot_is_used(cell.timeslot_offset)){
           //remove_links_to_schedule(&cell_list[0], cell_list_len-1);
